fd599e5941a02f7678513ecc47bb12045ba3028a
shengbin <2714971505@qq.com>
2026-01-14 10:13:29 +0800

    1

--- orion_fang/scripts/fang.sh
+++ orion_fang/scripts/fang.sh
@@ -1,6 +1,6 @@
 cd /vast/users/guangyi.chen/causal_group/zijian.li/dmir_crl/tab_model/orion_fang
 export PYTHONPATH=$PWD/src
-torchrun  --nproc_per_node=8   -m /vast/users/guangyi.chen/causal_group/zijian.li/dmir_crl/tab_model/orion_fang/src/orion_msp/train/run.py \
+torchrun  --nproc_per_node=8 /vast/users/guangyi.chen/causal_group/zijian.li/dmir_crl/tab_model/orion_fang/src/orion_msp/train/run.py \
             --wandb_log True \
             --wandb_project Orion-BiX \
             --wandb_name Stage1 \
